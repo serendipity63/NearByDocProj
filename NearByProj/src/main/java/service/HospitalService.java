52a7ac4700fd113c74b798ec3ef0705fd922114e
serendipity63 <jisuunwi@gmail.com>
2023-10-11 17:48:08 +0900

    update

--- NearByProj/src/main/java/service/HospitalService.java
+++ NearByProj/src/main/java/service/HospitalService.java
@@ -3,6 +3,12 @@ package service;
 import dto.Hospital;
 
 public interface HospitalService {
+	Hospital login(String comnum, String hpassword) throws Exception;
+
+	void hospitaljoin(Hospital hospital) throws Exception;
+	
+	String comnumcheck(String comnum) throws Exception;
+
 	Hospital hospitalHinfo(Integer num)throws Exception;
 	void HospitalModify(Hospital hospital) throws Exception;
 }
