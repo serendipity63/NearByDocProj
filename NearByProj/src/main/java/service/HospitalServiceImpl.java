8949bd561eb9c0ee6a6b0cfac42b077e689e4716
HsKim4054 <88170117+HsKim4054@users.noreply.github.com>
2023-10-16 20:24:17 +0900

    1016_04 김형섭

--- NearByProj/src/main/java/service/HospitalServiceImpl.java
+++ NearByProj/src/main/java/service/HospitalServiceImpl.java
@@ -1,6 +1,6 @@
 package service;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import dao.HospitalDao;
@@ -8,6 +8,8 @@ import dao.HospitalDaoImpl;
 import dto.Hospital;
 
 public class HospitalServiceImpl implements HospitalService {
+
+	static final double EARTH_RADIUS = 6371;
 
 	private HospitalDao hospitalDao;
 
@@ -48,8 +50,20 @@ public class HospitalServiceImpl implements HospitalService {
 	}
 
 	@Override
-	public List<Hospital> hospitalList() throws Exception {
-		return hospitalDao.selectHospitalList();
+	public List<Hospital> hospitalList(String department,String lat, String lon) throws Exception {
+		List<Hospital> hospitalList = hospitalDao.selectHospitalList(department);
+		for(Iterator<Hospital> iterator = hospitalList.iterator(); iterator.hasNext();) {
+			Hospital h = iterator.next();
+			double dlat=Double.parseDouble(lat);
+			double dlon=Double.parseDouble(lon);
+			double hlat=h.getLat().doubleValue();
+			double hlon=h.getLon().doubleValue();
+			double dis= getDistance(hlat,hlon,dlat,dlon);
+			if(dis>5000.0) {
+				iterator.remove();
+			}
+		}
+		return hospitalList;
 	}
 
 	@Override
@@ -63,4 +77,14 @@ public class HospitalServiceImpl implements HospitalService {
 		return null;
 	}
 
+	public static double getDistance(double lat1, double lon1, double lat2, double lon2) {
+		double dLat = Math.toRadians(lat2 - lat1);
+		double dLon = Math.toRadians(lon2 - lon1);
+
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double d = EARTH_RADIUS * c * 1000; // Distance in m
+		return d;
+	}
 }
