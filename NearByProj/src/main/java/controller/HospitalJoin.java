d7a438cd886bad6b494d94e2e78024a290e76e3e
serendipity63 <jisuunwi@gmail.com>
2023-10-17 09:22:18 +0900

    이미지 업로드 기능 추가

--- NearByProj/src/main/java/controller/HospitalJoin.java
+++ NearByProj/src/main/java/controller/HospitalJoin.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 import dto.Hospital;
 import service.HospitalService;
 import service.HospitalServiceImpl;
@@ -46,31 +49,38 @@ public class HospitalJoin extends HttpServlet {
 			throws ServletException, IOException {
 
 		request.setCharacterEncoding("utf-8");
-		String hname = request.getParameter("hname");
-		String hpassword = request.getParameter("hpassword");
-		String comnum = request.getParameter("comnum");
-		String htel = request.getParameter("htel");
-		String department = request.getParameter("department");
-		String lunch = request.getParameter("lunch");
-		String clinic = request.getParameter("clinic");
-		String hroad = request.getParameter("hroad");
-		String hdong = request.getParameter("hdong");
-		String hdetail = request.getParameter("hdetail");
-		String hpostcode = request.getParameter("hpostcode");
-		String hurl = request.getParameter("hurl");
-		BigDecimal hgrade =null;
-		Integer hreviewcnt=null ;
-		BigDecimal lat = new BigDecimal(request.getParameter("lat"));
+
+		String uploadPath = request.getServletContext().getRealPath("upload");
+		int size = 10 * 1024 * 1024;
+		MultipartRequest multi = new MultipartRequest(request, uploadPath, size, "utf-8",
+				new DefaultFileRenamePolicy());
+
+		String hname = multi.getParameter("hname");
+		String hpassword = multi.getParameter("hpassword");
+		String comnum = multi.getParameter("comnum");
+		String htel = multi.getParameter("htel");
+		String department = multi.getParameter("department");
+		String lunch = multi.getParameter("lunch");
+		String clinic = multi.getParameter("clinic");
+		String hroad = multi.getParameter("hroad");
+		String hdong = multi.getParameter("hdong");
+		String hdetail = multi.getParameter("hdetail");
+		String hpostcode = multi.getParameter("hpostcode");
+		String hurl= multi.getOriginalFileName("file");		
+		
+		System.out.println(hurl);
+		request.setAttribute("hurl", hurl);
+		
+		BigDecimal hgrade = null;
+		Integer hreviewcnt = null;
+		BigDecimal lat = new BigDecimal(multi.getParameter("lat"));
 		System.out.println(lat);
-		BigDecimal lon = new BigDecimal(request.getParameter("lon"));
+		BigDecimal lon = new BigDecimal(multi.getParameter("lon"));
 		System.out.println(lon);
-				
-				
-		
-		
 
 		Hospital hospital = new Hospital(hname, hpassword, comnum, htel, department, lunch, clinic, hroad, hdong,
 				hdetail, hpostcode, hurl, hgrade, hreviewcnt, lat, lon);
+		hospital.setHurl(hurl);
 		try {
 			HospitalService hospitalService = new HospitalServiceImpl();
 			hospitalService.hospitaljoin(hospital);
@@ -78,7 +88,6 @@ public class HospitalJoin extends HttpServlet {
 		} catch (Exception e) {
 			request.setAttribute("err", e.getMessage());
 			request.getRequestDispatcher("herror.jsp").forward(request, response);
-			// 에러 페이지 따로 만들거에요
 		}
 	}
 }
